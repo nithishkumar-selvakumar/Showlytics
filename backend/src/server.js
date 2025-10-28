7a3983cddf639eddfb42948610e69c01ce56f69c
S Nithishkumar <nithishkumar.s@soprasteria.com>
2025-10-28 11:42:41 +0530

    Server path changes and added logger

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,14 @@ import "dotenv/config"; // automatically loads .env variables
 import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
+import koaPinoLogger from "koa-pino-logger";
+import logger from "./logger/index.js";
 
 const app = new Koa();
 const router = new Router();
 
 app.use(bodyParser());
+app.use(koaPinoLogger({ logger }));
 
 router.get("/", (ctx) => {
   ctx.body = { message: "Hello Koa!", port: process.env.PORT };
@@ -15,4 +18,6 @@ router.get("/", (ctx) => {
 app.use(router.routes()).use(router.allowedMethods());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  logger.info(`Server running on http://localhost:${PORT}`),
+);
